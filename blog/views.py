f80e17b7f8ec4b5c288f58a7ef5b091179405232
administrateur <mkraiem010@gmail.com>
2016-11-08 11:01:49 +0100

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.shortcuts import render
 from django.utils import timezone
 from .forms import PostForm
+from django.shortcuts import redirect
 
 from .models import Post
 
